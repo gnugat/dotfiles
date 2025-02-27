df752d640d51fd27b38c37e50a77ed6a7186b19e
Loïc Faugeron <faugeron.loic@gmail.com>
2025-02-27 18:36:02 +0000

    [quarry] Added new-package block
    Used the block `new-quarry-block` to create the `new-package` block:
    
    ```console
    _SSDF_INPUT_BLOCK_ID='0a01' \
        _SSDF_INPUT_BLOCK_NAME='new-package' \
        _SSDF_INPUT_BLOCK_EMOJI='🪨🎁' \
        _SSDF_INPUT_BLOCK_TITLE='New Package' \
        _SSDF_INPUT_BLOCK_SHORT_DESCRIPTION='Adds support for a new
    package.' \
        bash
    ~/.dotfiles/00-_ssdf/quarry/0a00-new-quarry-block/1.0/install.sh
    ```
    
    This is eating our own dog food. THIS IS TRUE META OF STEAL 🤘 (steal!).

---
+++ 00-_ssdf/quarry/0a01-new-package/1.0/templates/config/envvars.package.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# File: /{{ _SSDF_INPUT_PACKAGE_ID }}-{{ _SSDF_INPUT_PACKAGE_NAME }}/config/envvars.{{ _SSDF_INPUT_PACKAGE_NAME }}.sh
+# ──────────────────────────────────────────────────────────────────────────────
+# ♻️ {{ _SSDF_INPUT_PACKAGE_EMOJI }} ENVVAR management, for {{ _SSDF_INPUT_PACKAGE_NAME }}.
+# ──────────────────────────────────────────────────────────────────────────────
+
