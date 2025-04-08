ff2a9854e6aea081cd1e90adfa9d062897410bbe
Loïc Faugeron <faugeron.loic@gmail.com>
2025-04-08 17:57:58 +0100

    [SketchyBar] Fixed theme colours in plugins (slack)

--- 53-sketchybar/config/plugins/slack.sh
+++ 53-sketchybar/config/plugins/slack.sh
@@ -3,9 +3,13 @@
 # ──────────────────────────────────────────────────────────────────────────────
 
 ## ─────────────────────────────────────────────────────────────────────────────
+## ☕ Theme: homemade Catpuccin Machiatto
+## ─────────────────────────────────────────────────────────────────────────────
+source "${CONFIG_DIR}/themes/catppuccin-macchiato.sh"
+
+## ─────────────────────────────────────────────────────────────────────────────
 ## Displays numbder of Slack direct unread notifications.
 ## ─────────────────────────────────────────────────────────────────────────────
 _SSDF_SB_SLACK_STATUS_LABEL=$(lsappinfo info -only StatusLabel Slack | sed -n 's/.*"label"="\(.*\)".*/\1/p')
 
-                                                                           # ↓ Using envvar ($_SSDF_CM_RED) doesn't work there :/
-sketchybar --set "$NAME" label="${_SSDF_SB_SLACK_STATUS_LABEL}" label.color="0xfff38ba8"
+sketchybar --set "$NAME" label="${_SSDF_SB_SLACK_STATUS_LABEL}" label.color="${_SSDF_CM_RED}"
