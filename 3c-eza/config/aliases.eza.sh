2a7e31dee212f5c75d24c004242479b8c6e97951
Loïc Faugeron <faugeron.loic@gmail.com>
2025-07-03 10:42:27 +0100

    [eza] Fixing the sorting (`3a-*` should be after `39-*`)

--- 3c-eza/config/aliases.eza.sh
+++ 3c-eza/config/aliases.eza.sh
@@ -6,9 +6,31 @@
 
 ## ─────────────────────────────────────────────────────────────────────────────
 ## 📃 Listing.
+##
+## NOTE: eza's built-in sorting is lexicographic (sorts `3a-*` before `30-*`).
+## We have to pipe `eza` to `sort -n` to instead have numeric ordering.
+## To make sure the directories are displayed before the files,
+## even when the icon is displayed (`sort` won't ignore the UTF-8 character),
+## we split the command in two.
 ## ─────────────────────────────────────────────────────────────────────────────
-alias la='eza --color=always --tree --level=0 --icons=always --all'
-alias ll='eza --color=always --tree --level=0 --icons=always --long --group --git'
-alias lla='eza --color=always --tree --level=0 --icons=always --all --long --group --git'
-alias l='eza --color=always --tree --level=0 --icons=always'
+
+# Remove any existing aliases
+unalias l la ll lla 2>/dev/null || true
+
+l() {
+    eza --color=always --tree --level=0 --icons=always --only-dirs "$@" | sort -n
+    eza --color=always --tree --level=0 --icons=always --only-files "$@" | sort -n
+}
+
+la() {
+    l --all
+}
+
+ll() {
+    l --long --group --git
+}
+
+lla() {
+    l --all --long --group --git
+}
 
