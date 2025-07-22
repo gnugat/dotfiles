79b4c7303acb28c958ebbf4e2e765fa9b7007c0e
Loïc Faugeron <faugeron.loic@gmail.com>
2025-07-22 21:06:45 +0100

    [26-git-crypt] Installed git-crypt, for totally no reasons

---
+++ 26-git-crypt/_brew.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# File: /26-git-crypt/_brew.sh
+# ──────────────────────────────────────────────────────────────────────────────
+# 📰🍏🔒 Installing git-crypt, on Mac OS (brew).
+# ──────────────────────────────────────────────────────────────────────────────
+
+HOMEBREW_NO_ENV_HINTS=1 brew install -q git-crypt
