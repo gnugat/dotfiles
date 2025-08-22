6ad3eeedd5fa0a6f83fe1f5bbdd3edd8cd1675e1
Loïc Faugeron <faugeron.loic@gmail.com>
2025-08-22 13:58:24 +0100

    [tmux] Fixed plugins installation

--- 3f-tmux/install.sh
+++ 3f-tmux/install.sh
@@ -42,7 +42,15 @@ ln -nsf \
 mkdir -p "${HOME}/.config/tmux/plugins"
 if [ ! -e "${HOME}/.config/tmux/plugins/tpm" ]; then
     git clone https://github.com/tmux-plugins/tpm "${HOME}/.config/tmux/plugins/tpm"
+    bash "${HOME}/.config/tmux/plugins/tpm/bin/install_plugins" all
 fi
+bash "${HOME}/.config/tmux/plugins/tpm/bin/update_plugins" all
+
+## Install Catppuccin theme manually (recommended approach)
+if [ ! -e "${HOME}/.config/tmux/plugins/catppuccin" ]; then
+    git clone https://github.com/catppuccin/tmux "${HOME}/.config/tmux/plugins/catppuccin"
+fi
+(cd "${HOME}/.config/tmux/plugins/catppuccin" && git pull)
 
 _ssdf_echo_success "${_SSDF_PACKAGE_NAME} installed"
 
