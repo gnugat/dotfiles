5d38722c997ab6ad2b488d8f91fd4b6f4770e342
Loïc Faugeron <faugeron.loic@gmail.com>
2025-09-16 07:35:11 +0100

    [spotify] Installed Spotify for nui-rama

---
+++ 64-spotify/_brew.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# File: /64-spotify/_brew.sh
+# ──────────────────────────────────────────────────────────────────────────────
+# 📰🍏🎵 Installing spotify, on Mac OS (brew).
+# ──────────────────────────────────────────────────────────────────────────────
+
+HOMEBREW_NO_ENV_HINTS=1 brew install -q --cask spotify
