d14c72bcb6d0c10c7638ac85a6dab4160ccf9dc5
Loïc Faugeron <faugeron.loic@gmail.com>
2026-07-28 15:32:44 +0200

    vim[lsp]: replaced elixir-ls with expert

--- 203-vim/_brew.sh
+++ 203-vim/_brew.sh
@@ -4,4 +4,4 @@
 # 📰🍏✌️ Installing vim, on Mac OS (brew).
 # ─────────────────────────────────────────────────────────────────────────────
 
-HOMEBREW_NO_ENV_HINTS=1 brew install -q vim elixir-ls rust-analyzer
+HOMEBREW_NO_ENV_HINTS=1 brew install -q vim expert rust-analyzer
